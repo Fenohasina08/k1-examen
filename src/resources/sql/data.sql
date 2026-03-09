50faf8f4732d5fd138c537a6d1e1b0300f3a3ebb
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:46:36 +0300

    feat: requetes num 2

--- src/resources/sql/data.sql
+++ src/resources/sql/data.sql
@@ -7,3 +7,10 @@ FROM Reparation
 GROUP BY Modele_voiture.marque, Modele_voiture.modele
 ORDER BY Modele_voiture.marque, Modele_voiture.modele;
 
+SELECT
+    ROUND(100.0 * SUM(CASE WHEN Modele_voiture.modele = 'RANGER' THEN 1 ELSE 0 END) / COUNT(*), 2) AS nbre_reparation_ranger,
+    ROUND(100.0 * SUM(CASE WHEN Modele_voiture.modele = 'EVEREST' THEN 1 ELSE 0 END) / COUNT(*), 2) AS nbre_reparation_everest,
+    ROUND(100.0 * SUM(CASE WHEN Modele_voiture.modele = 'YUKON' THEN 1 ELSE 0 END) / COUNT(*), 2) AS nbre_reparation_yukon,
+    ROUND(100.0 * SUM(CASE WHEN Modele_voiture.modele = 'RAM' THEN 1 ELSE 0 END) / COUNT(*), 2) AS nbre_reparation_ram
+FROM Reparation
+         JOIN Modele_voiture ON Reparation.id_modele_voiture = Modele_voiture.id;
