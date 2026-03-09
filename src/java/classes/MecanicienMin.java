9595a39585e58ef524279ed06d639880716aed20
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:56:48 +0300

    feat: add content of CoutParMecanicien  files

---
+++ src/java/classes/MecanicienMin.java
@@ -0,0 +1,30 @@
+public class CoutParMecanicien {
+
+    private String marque;
+    private String nomMecanicien;
+    private double coutReparation;
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public void setNomMecanicien(String nomMecanicien) {
+        this.nomMecanicien = nomMecanicien;
+    }
+
+    public void setCoutReparation(double coutReparation) {
+        this.coutReparation = coutReparation;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public String getNomMecanicien() {
+        return nomMecanicien;
+    }
+
+    public double getCoutReparation() {
+        return coutReparation;
+    }
+}
