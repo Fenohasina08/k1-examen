71f309e93e5950f8d47d7036fe7d88c09b6d9ca2
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:31:44 +0300

    feat: refonte des contents

---
+++ src/java/classes/DataRetriever.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DataRetriever {
+    private DBConnection dbConnection;
+
+    public DataRetriever(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+ }
+}
+
---
+++ src/java/classes/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DBConnection db = new DBConnection();
+        DataRetriever retriever = new DataRetriever(db);
+
+    }
+}
