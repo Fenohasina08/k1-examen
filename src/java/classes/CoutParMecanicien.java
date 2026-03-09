df4174970cc4e02508da8f46fd19adac86a03b0d
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:56:16 +0300

    feat: add content of MecanicienMin  files

---
+++ src/java/classes/CoutParMecanicien.java
@@ -0,0 +1,21 @@
+public class MecanicienMin {
+
+    private String nomMecanicien;
+    private double coutReparationRapporte;
+
+    public void setNomMecanicien(String nomMecanicien) {
+        this.nomMecanicien = nomMecanicien;
+    }
+
+    public void setCoutReparationRapporte(double coutReparationRapporte) {
+        this.coutReparationRapporte = coutReparationRapporte;
+    }
+
+    public String getNomMecanicien() {
+        return nomMecanicien;
+    }
+
+    public double getCoutReparationRapporte() {
+        return coutReparationRapporte;
+    }
+}
