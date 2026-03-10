5557fe2939c22d1df681bc1e563796c63e16331e
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-10 10:06:57 +0300

    feat: add hashcode

--- src/java/classes/MecanicienMin.java
+++ src/java/classes/MecanicienMin.java
@@ -27,4 +27,15 @@ public class CoutParMecanicien {
     public double getCoutReparation() {
         return coutReparation;
     }
+
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        CoutParMecanicien that = (CoutParMecanicien) object;
+        return java.lang.Double.compare(coutReparation, that.coutReparation) == 0 && java.util.Objects.equals(marque, that.marque) && java.util.Objects.equals(nomMecanicien, that.nomMecanicien);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), marque, nomMecanicien, coutReparation);
+    }
 }
